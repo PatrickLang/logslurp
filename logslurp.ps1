448f26e36fb6e4f92df620c7ccd11d4cc34ff7cc
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2018-07-18 14:23:09 +0000

    Add non interactive login option

--- logslurp.ps1
+++ logslurp.ps1
@@ -1,7 +1,17 @@
+Param(
+  [string]$win_user,
+  [string]$win_pass
+)
 
 $lockedFiles = "kubelet.err.log", "kubelet.log", "kubeproxy.log", "kubeproxy.err.log"
-
-$cred = Get-Credential -Message "Please enter an admin username & password to connect to the Windows nodes"
+if (($PSBoundParameters.ContainsKey("win_user")) -and ($PSBoundParameters.ContainsKey("win_pass")))
+{ 
+   $passwd = ConvertTo-SecureString $win_pass -AsPlainText -Force
+   $cred = New-Object System.Management.Automation.PSCredential ($win_user, $passwd) 
+}
+else {
+   $cred = Get-Credential -Message "Please enter an admin username & password to connect to the Windows nodes"
+}
 $nodes = ./kubectl get node -o json | ConvertFrom-Json
 $nodes.items | Where-Object { $_.metadata.labels.'beta.kubernetes.io/os' -eq 'windows' } | foreach-object {
   Add-Member -InputObject $_ -MemberType NoteProperty -Name "pssession" -Value (New-PSSession -ComputerName $_.status.nodeInfo.machineID -Credential $cred -UseSSL -Authentication basic)

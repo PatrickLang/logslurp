f4aabb5e8342f1754c597402a31c54d7d8776dcd
Patrick Lang <plang@microsoft.com>
2018-08-02 14:15:23 -0700

    Adding hns log collection

--- logslurp.ps1
+++ logslurp.ps1
@@ -3,7 +3,11 @@ Param(
   [string]$win_pass
 )
 
+# Constants
 $lockedFiles = "kubelet.err.log", "kubelet.log", "kubeproxy.log", "kubeproxy.err.log"
+$netDebugFiles = "network.txt", "endpoint.txt", "policy.txt", "ip.txt", "ports.txt", "routes.txt"
+
+# Handle credentials as parameters, else prompt for them
 if (($PSBoundParameters.ContainsKey("win_user")) -and ($PSBoundParameters.ContainsKey("win_pass")))
 { 
    $passwd = ConvertTo-SecureString $win_pass -AsPlainText -Force
@@ -12,6 +16,8 @@ if (($PSBoundParameters.ContainsKey("win_user")) -and ($PSBoundParameters.Contai
 else {
    $cred = Get-Credential -Message "Please enter an admin username & password to connect to the Windows nodes"
 }
+
+
 $nodes = ./kubectl get node -o json | ConvertFrom-Json
 $nodes.items | Where-Object { $_.metadata.labels.'beta.kubernetes.io/os' -eq 'windows' } | foreach-object {
   Add-Member -InputObject $_ -MemberType NoteProperty -Name "pssession" -Value (New-PSSession -ComputerName $_.status.nodeInfo.machineID -Credential $cred -UseSSL -Authentication basic)
@@ -23,7 +29,12 @@ $nodes.items | Where-Object { $_.metadata.labels.'beta.kubernetes.io/os' -eq 'wi
     $paths += $using:lockedFiles | Foreach-Object { Copy-Item "c:\k\$_" . -Passthru }
     get-eventlog -LogName System -Source "Service Control Manager" -Message *kub* | ft Index, TimeGenerated, EntryType, Message | out-file "$ENV:TEMP\\services.log"
     $paths += "$ENV:TEMP\\services.log"
+    Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/Microsoft/SDN/master/Kubernetes/windows/debug/collectlogs.ps1 -OutFile c:\k\debug\collectlogs.ps1
+    & c:\k\debug\collectlogs.ps1
+    $netLogs = get-childitem c:\k -Recurse -Include $using:netDebugFiles
+    $paths += $netLogs
     Compress-Archive -Path $paths -DestinationPath $using:zipName
+    $netLogs | Foreach-Object { Remove-Item $_ }
     Get-ChildItem $using:zipName
   } 
   Copy-Item -FromSession $_.pssession $remoteZipPath -Destination out/
